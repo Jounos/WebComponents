e5bb24b2a4db7affb270a834f4a74cdc958ba955
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-10-02 08:58:22 -0300

    Iniciado a refatoração do component

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -3,7 +3,41 @@ class CardNews extends HTMLElement {
     super();
 
     const shadow = this.attachShadow({ mode: 'open' });
-    shadow.innerHTML = '<h1>Hello World</h1>'
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+  }
+
+  build() {
+    const componentRoot = document.createElement('div');
+    componentRoot.setAttribute('class', 'card');
+    
+    const cardLeft = document.createElement('div');
+    componentRoot.setAttribute('class', 'card-left');
+    
+    const autor = document.createElement('span');
+    autor.textContent = 'By ' + this.getAttribute("autor");
+    
+    const linkTitle = document.createElement('a');
+    const paragrafo = documento.createElement('p');
+    
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(paragrafo);
+
+    const cardRight = document.createElement('div');
+    componentRoot.setAttribute('class', 'card-right');
+
+    const newsImage = document.createAttribute('img');
+    cardRight.appendChild(newsImage);
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    return componentRoot;
+  }
+
+  styles() {
+
   }
 }
 
